69bd2c47426af6e9b36520b51258ce948b685826
Luke Bemish <lukebemish@lukebemish.dev>
2024-01-15 15:27:00 -0600

    [no ci] change how module tests are skipped

--- testtargets/src/test/java/dev/lukebemish/opensesame/test/java/Open/TestModules.java
+++ testtargets/src/test/java/dev/lukebemish/opensesame/test/java/Open/TestModules.java
@@ -1,13 +1,14 @@
 package dev.lukebemish.opensesame.test.java.Open;
 
 import dev.lukebemish.opensesame.annotations.Open;
-import org.junit.jupiter.api.Tag;
+import dev.lukebemish.opensesame.test.target.Public;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Tag("modules")
+@DisabledIf("isNotModular")
 public class TestModules {
     @Open(
             name = "hiddenByModules",
@@ -37,5 +38,9 @@ public class TestModules {
             Class<?> hidden = Class.forName("dev.lukebemish.opensesame.test.target.hidden.Hidden");
             hidden.getDeclaredMethod("hiddenByModules").invoke(null);
         });
+    }
+
+    private static boolean isNotModular() {
+        return !Public.class.getModule().isNamed();
     }
 }
