ccc84baff04459a909369143a6bbcd9144f60483
Luke Bemish <lukebemish@lukebemish.dev>
2024-01-15 12:19:47 -0600

    Change plugin application

--- plugin/loom/src/main/java/dev/lukebemish/opensesame/plugin/loom/OpenSesamePluginLoom.java
+++ plugin/loom/src/main/java/dev/lukebemish/opensesame/plugin/loom/OpenSesamePluginLoom.java
@@ -1,16 +1,21 @@
 package dev.lukebemish.opensesame.plugin.loom;
 
-import dev.lukebemish.opensesame.plugin.OpenSesameExtension;
 import dev.lukebemish.opensesame.plugin.OpenSesamePlugin;
+import net.fabricmc.loom.api.LoomGradleExtensionAPI;
+import net.fabricmc.loom.api.remapping.RemapperParameters;
+import net.fabricmc.loom.bootstrap.LoomGradlePluginBootstrap;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class OpenSesamePluginLoom implements Plugin<Project> {
-    @Override
+
+    @Override@SuppressWarnings({"unchecked", "rawtypes"})
     public void apply(Project target) {
         target.getPlugins().apply(OpenSesamePlugin.class);
+        target.getPlugins().apply(LoomGradlePluginBootstrap.class);
 
-        var openSesameExtension = (OpenSesameExtension) target.getExtensions().getByName("opensesame");
-        openSesameExtension.getExtensions().create("loom", LoomExtension.class);
+        LoomGradleExtensionAPI loomExtension = (LoomGradleExtensionAPI) target.getExtensions().getByName("loom");
+        loomExtension.addRemapperExtension((Class) OpeningRemapperExtension.class, RemapperParameters.None.class, i -> {});
+        loomExtension.getKnownIndyBsms().add("dev/lukebemish/opensesame/runtime/OpeningMetafactory");
     }
 }
