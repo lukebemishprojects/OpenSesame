f57f2d4d45d7dc38e80724557467e29320ee0cbb
Luke Bemish <lukebemish@lukebemish.dev>
2024-01-27 02:17:49 -0600

    Constructor clarifications

--- core/src/main/java/dev/lukebemish/opensesame/annotations/extend/Constructor.java
+++ core/src/main/java/dev/lukebemish/opensesame/annotations/extend/Constructor.java
@@ -6,12 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks a constructor in an interface marked with {@link Extend} as the constructor for new subclass. The constructor's signature
- * must match a constructor in the target class, or the no-arg constructor if the target is an interface. {@link dev.lukebemish.opensesame.annotations.Coerce}
+ * Marks a method in an interface marked with {@link Extend} as a constructor for the new subclass. The method's signature
+ * must match a constructor in the target class, or have no arguments if the target is an interface. {@link dev.lukebemish.opensesame.annotations.Coerce}
  * may be used if the types are inaccessible. Optionally, the normal constructor parameters may be preceded by one
  * or more {@link Field} parameters; the corresponding fields of the generated class will be set after the constructor
  * of the superclass is called, in the constructor of the generated class corresponding to this method. The annotated
- * method must be static.
+ * method must be static, and must return exactly the type of the interface.
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
