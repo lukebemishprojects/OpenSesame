57a35dcb294f7e2a259ddb9a854635e37c5b0d1d
Luke Bemish <lukebemish@lukebemish.dev>
2024-07-15 17:49:36 -0500

    Fix i383 windows compiler

--- toolchains/windows-i386.cmake
+++ toolchains/windows-i386.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME  Windows)
-set(CMAKE_C_COMPILER   i686-w64-mingw32-g++-win32)
+set(CMAKE_C_COMPILER   i686-w64-mingw32-gcc-win32)
 set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++-win32)
 set(CMAKE_C_FLAGS      "-m32 -march=i386")
 set(CMAKE_CXX_FLAGS    "-m32 -march=i386")
