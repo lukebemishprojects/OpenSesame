1adfd0c6fb1b6cf083a755091ac9712838ea5b4c
Luke Bemish <lukebemish@lukebemish.dev>
2024-07-15 16:31:35 -0500

    Fix various issues

--- toolchains/linux-aarch64.cmake
+++ toolchains/linux-aarch64.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME  Linux)
 set(CMAKE_C_COMPILER   aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-set(CMAKE_C_FLAGS      "-m64 -march=armv8-a")
-set(CMAKE_CXX_FLAGS    "-m64 -march=armv8-a")
+set(CMAKE_C_FLAGS      "-march=armv8-a")
+set(CMAKE_CXX_FLAGS    "-march=armv8-a")
