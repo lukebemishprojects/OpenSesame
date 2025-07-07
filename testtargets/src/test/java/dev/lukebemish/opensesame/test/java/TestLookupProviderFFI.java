fd3792ced68ec44e85ea1dac865b21a9b1047fc3
Luke Bemish <lukebemish@lukebemish.dev>
2025-07-07 03:09:05 -0500

    [no ci] add test for new provider specifically

---
+++ testtargets/src/test/java/dev/lukebemish/opensesame/test/java/TestLookupProviderFFI.java
@@ -0,0 +1,43 @@
+package dev.lukebemish.opensesame.test.java;
+
+import dev.lukebemish.opensesame.annotations.Open;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+
+import java.lang.invoke.MethodHandles;
+
+@DisabledIf("isFFIProviderAvailable")
+public class TestLookupProviderFFI {
+    @Open(
+            name = "openingLookup",
+            targetName = "dev.lukebemish.opensesame.runtime.LookupProviderFFI",
+            type = Open.Type.VIRTUAL,
+            unsafe = true
+    )
+    private static MethodHandles.Lookup invokeOpeningLookup(Object provider, MethodHandles.Lookup original, Class<?> target) {
+        throw new AssertionError("Method not transformed");
+    }
+
+    @Open(
+            name = "<init>",
+            targetName = "dev.lukebemish.opensesame.runtime.LookupProviderFFI",
+            type = Open.Type.CONSTRUCT,
+            unsafe = true
+    )
+    private static Object createLookupProvider() {
+        throw new AssertionError("Method not transformed");
+    }
+    
+    @Test
+    void lookupHasOriginalAccess() throws Throwable {
+        var provider = createLookupProvider();
+        var lookup = invokeOpeningLookup(provider, MethodHandles.lookup(), TestLookupProviderFFI.class);
+        // Test if it is IMPL_LOOKUP, by using it to grab IMPL_LOOKUP
+        var getter = lookup.findStaticGetter(MethodHandles.Lookup.class, "IMPL_LOOKUP", MethodHandles.Lookup.class);
+        getter.invoke();
+    }
+
+    private static boolean isFFIProviderAvailable() {
+        return Runtime.version().feature() >= 22;
+    }
+}
